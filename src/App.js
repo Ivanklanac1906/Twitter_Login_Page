0537702dd3572816a4f369d93139acd0e0e73e03
Ivanklanac1906 <iklanac954@gmail.com>
2024-10-26 00:29:46 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ function App() {
    <div className='logo-box'>
   <img src='./x.png' alt='logo' className='logo'/>
   <h2>Sign In To X</h2>
+  <button>
+  <img src='./google.png' alt='logo'/>
+  Sign in with Google
+  </button>
+  <button>
+  <img src='./applel.jpg' alt='logo'/>
+  Sign in with Google
+  </button>
+
+  <hr></hr>
+
+  <span>Or</span>
+
+  <form>
+    <input type = "text" placeholder='Phone email or username'/>
+    <button>Next</button>
+  </form>
  </div>
     </div>
   );
